49c5e4cdbbeda09405f99c0500d41a17ca1ccbd0
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-07-20 07:43:35 -0400

    add cube.h

---
+++ cube.h
@@ -0,0 +1,16 @@
+#define FACE_COLOR_RED      0
+#define FACE_COLOR_ORANGE   1
+#define FACE_COLOR_YELLOW   2
+#define FACE_COLOR_GREEN    3
+#define FACE_COLOR_BLUE     4
+#define FACE_COLOR_PURPLE   5
+#define NUM_FACES           6
+
+#define FACE_WIDTH 3
+#define FACE_HEIGHT 3
+
+#define NUM_SQUARES (NUM_FACES * FACE_WIDTH * FACE_HEIGHT)
+
+#define TWIST_CLOCKWISE         0
+#define TWIST_COUNTERCLOCKWISE  1
+#define NUM_TWISTS              2
